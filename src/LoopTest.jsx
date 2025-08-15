db03d272b333ee16b811dce535aec0902c51ed3c
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2025-08-15 16:25:47 +0300

    Added conditionals to check if animals exists and the length of the list

--- src/LoopTest.jsx
+++ src/LoopTest.jsx
@@ -1,13 +1,24 @@
-function ListItems(props) {
-  return <li>{props.animal}</li>;
-}
+// function ListItems(props) {
+//   return <li>{props.animal}</li>;
+// }
 
 function List(props) {
+  if (!props.animals) {
+    return <div>Loading... </div>;
+  }
+
+  if (props.animals.length === 0) {
+    return <div>There are no animals in the list!</div>;
+  }
   return (
     <ul>
-      {props.animals.map((animal) => (
-        <ListItems key={animal} animal={animal} />
-      ))}
+      {props.animals.map(
+        (animal) => {
+          //   return animal.startsWith("L") ? <li key={animal}>{animal}</li> : null;
+          return animal.startsWith("L") && <li key={animal}>{animal}</li>;
+        }
+        //   (<ListItems key={animal} animal={animal} />)
+      )}
     </ul>
   );
 }
