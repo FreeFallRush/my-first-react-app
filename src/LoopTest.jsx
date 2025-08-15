116d516d7b69ef839f33ccd82d2f0bdc41dcae3a
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2025-08-15 15:09:38 +0300

    Added animalList in LoopTest.jsx and arranged the code

--- src/LoopTest.jsx
+++ src/LoopTest.jsx
@@ -1,14 +1,11 @@
 function LoopTest() {
   const animals = ["Lion", "Cow", "Snake", "Lizard"];
+  const animalList = animals.map((animal) => <li key={animal}>{animal}</li>);
 
   return (
     <>
       <h3>Animals: </h3>
-      <ul>
-        {animals.map((animal) => (
-          <li key={animal}>{animal}</li>
-        ))}
-      </ul>
+      <ul>{animalList}</ul>
     </>
   );
 }
